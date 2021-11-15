11f3fed671653300cf6cde11bd6d63b0d175f570
KamilKamilK <k.kaczmarek@dotandspot.pl>
2021-11-15 19:28:39 +0100

    order model was created

---
+++ database/migrations/2021_11_15_180429_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->decimal('price', 5,2)->default(0.00);
+            $table->enum('order_status', ['open', 'close'])->default('open');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
